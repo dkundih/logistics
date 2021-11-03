536d5376aa581d41db4b008dac7ac44755e19f0a
David Kundih <kundihdavid@gmail.com>
2021-11-04 00:18:37 +0100

    m

--- unin/__init__.py
+++ unin/__init__.py
@@ -3,6 +3,13 @@ from duality import montecarlo
 from duality import eoq
 from duality import hub
 from duality.misc import global_functions
+from misc._meta import (
+	__author__,
+	__copyright__,
+	__credits__,
+	__license__,
+	__version__,
+)
 
 import pandas as pd
 import matplotlib.pyplot as plt
