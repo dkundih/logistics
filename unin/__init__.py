4238cf8e6fa6e1c058ff815cdf47ed6b10527287
David Kundih <kundihdavid@gmail.com>
2021-11-04 00:03:31 +0100

    start

--- unin/__init__.py
+++ unin/__init__.py
@@ -1,8 +1,9 @@
 #imports essential functions from the unin module.
-from unin.eoq import eoq
-from unin.hub import hub
-from unin.misc.global_functions import *
-from unin.montecarlo import montecarlo
+from duality import montecarlo
+from duality import eoq
+from duality import hub
+from duality.misc import global_functions
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import random
