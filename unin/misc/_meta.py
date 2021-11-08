b3aaf2f566faddfba2e1c26c004accff00203ea1
David Kundih <kundihdavid@gmail.com>
2021-11-08 12:19:34 +0100

    _meta change

--- unin/misc/_meta.py
+++ unin/misc/_meta.py
@@ -9,11 +9,11 @@ __credits__ = [
 	'dkundih <https://github.com/dkundih>',
 
 ]
-__license__ = 'Copyright (c) 2021- David Kundih. All rights reserved.\nLicensed under the Apache License, Version 2.0.\n\nFor more details about the license and terms of use visit the official unin documentation linked at https://github.com/dkundih/unin and https://pypi.org/project/unin'
+__license__ = 'Copyright (c) 2021- David Kundih. All rights reserved.\nLicensed under the Apache License, Version 2.0 | For more details about the license and terms of use visit the official unin documentation linked at https://github.com/dkundih/unin and https://pypi.org/project/unin'
 __version__ = '1.0.5'
-__documentation__ = 'https://github.com/dkundih/unin\nhttps://pypi.org/project/unin'
-__contact__ = 'dakundih@unin.hr\nkundihdavid@gmail.com'
-__donate__ = 'https://patreon.com/dkundih\nhttps://buymeacoffee.com/dkundih'
+__documentation__ = 'https://github.com/dkundih/unin | https://pypi.org/project/unin'
+__contact__ = 'dakundih@unin.hr | kundihdavid@gmail.com'
+__donate__ = 'https://patreon.com/dkundih | https://buymeacoffee.com/dkundih'
 
 #general setup confirmation.
 def main():
