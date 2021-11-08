0202ffbbe65fea46168b9b15a4819ce0787233f5
David Kundih <kundihdavid@gmail.com>
2021-11-08 13:06:20 +0100

    n change

--- unin/misc/_meta.py
+++ unin/misc/_meta.py
@@ -9,7 +9,7 @@ __credits__ = [
 	'dkundih <https://github.com/dkundih>',
 
 ]
-__license__ = 'Copyright © 2021- David Kundih. All rights reserved.\nLicensed under the Apache License, Version 2.0 | For more details about the license and terms of use visit the official unin documentation linked at https://github.com/dkundih/unin and https://pypi.org/project/unin'
+__license__ = 'Copyright © 2021- David Kundih. All rights reserved. Licensed under the Apache License, Version 2.0 | For more details about the license and terms of use visit the official unin documentation linked at https://github.com/dkundih/unin and https://pypi.org/project/unin'
 __version__ = '1.0.5'
 __documentation__ = 'https://github.com/dkundih/unin | https://pypi.org/project/unin'
 __contact__ = 'dakundih@unin.hr | kundihdavid@gmail.com'
