440697e6cf314bc688830ff8ce3909f94567bdf9
David Kundih <kundihdavid@gmail.com>
2022-07-28 03:29:12 +0200

    meta

--- logistics/misc/_meta.py
+++ logistics/misc/_meta.py
@@ -11,8 +11,8 @@ __credits__ = [
 	'dkundih <https://github.com/dkundih>',
 
 ]
-__license__ = 'Copyright © 2021- David Kundih. All rights reserved. Licensed under the Apache License, Version 2.0 | For more details about the license and terms of use visit the official unin documentation linked at https://github.com/dkundih/unin and https://pypi.org/project/unin'
+__license__ = 'Copyright © 2021- David Kundih. All rights reserved. Licensed under the Apache License, Version 2.0 | For more details about the license and terms of use visit the official unin documentation linked at https://github.com/dkundih/logistics and https://pypi.org/project/logistics'
 __version__ = '0.0.11'
-__documentation__ = 'https://github.com/dkundih/unin | https://pypi.org/project/unin'
+__documentation__ = 'https://github.com/dkundih/logistics | https://pypi.org/project/logistics'
 __contact__ = 'dakundih@unin.hr | kundihdavid@gmail.com'
 __donate__ = 'https://patreon.com/dkundih | https://buymeacoffee.com/dkundih'
